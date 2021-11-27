7aae59414947cd1a02ec9b3a994b2b78e273f0ac
martinvrbka <martin.vrbkaa@seznam.cz>
2021-11-27 18:01:30 +0100

    Initial text strings

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Bulls and Cows project
+def main():
+    number = 9999
+    print(
+        "Hi There!",
+        "=" * 47,
+        "I've generated a random 4 digit number for you.",
+        "Let's play a bulls and cows game.",
+        sep="\n"
+    )
+    pass
